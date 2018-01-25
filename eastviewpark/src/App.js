dbd63ebd30f573497a91a475e635605a949a0d7d
ttejuosho <theycallmeflowz@yahoo.com>
2018-01-25 14:07:03 -0600

    fonts in

--- eastviewpark/src/App.js
+++ eastviewpark/src/App.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './materialize/css/materialize.css';
+import './materialize/fonts/roboto/Roboto-Bold.woff';
+// import Header from './components/Header.js';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <nav class="nav-extended">
+        <div class="nav-wrapper">
+        <a href="#!" class="brand-logo">Logo</a>
+        <ul class="right hide-on-med-and-down">
+            <li><a>A link</a></li>
+            <li><a>A second link</a></li>
+            <li><a>A third link</a></li>
+        </ul>
+        </div>
+        <div class="nav-content">
+        <span class="nav-title">Title</span>
+        <a class="btn-floating btn-large halfway-fab waves-effect waves-light teal">
+            <i class="material-icons">add</i>
+        </a>
+        </div>
+    </nav>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+       
       </div>
     );
   }
--- eastviewpark/src/components/Header.js
+++ eastviewpark/src/components/Header.js
@@ -1,14 +1,31 @@
 import React, { Component } from "react";
 import '../App.css';
 import './materialize/css/materialize.css';
+import './materialize/css/materialize.min.css';
 import './materialize/js/materialize.js';
+import './materialize/js/materialize.min.js';
 
 
 class Header extends Component {
     render(){
         return(
             <div>
-
+                <nav class="nav-extended">
+                    <div class="nav-wrapper">
+                    <a href="#!" class="brand-logo">Logo</a>
+                    <ul class="right hide-on-med-and-down">
+                        <li><a>A link</a></li>
+                        <li><a>A second link</a></li>
+                        <li><a>A third link</a></li>
+                    </ul>
+                    </div>
+                    <div class="nav-content">
+                    <span class="nav-title">Title</span>
+                    <a class="btn-floating btn-large halfway-fab waves-effect waves-light teal">
+                        <i class="material-icons">add</i>
+                    </a>
+                    </div>
+                </nav>
             </div>
         )
     }
