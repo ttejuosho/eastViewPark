02b3a2c21cefc5761ef949dba0bc6bf3408b06f8
ttejuosho <theycallmeflowz@yahoo.com>
2018-01-25 12:40:38 -0600

    Components in

---
+++ eastviewpark/src/components/ServReq.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import '../App.css';
+import './materialize/css/materialize.css';
+import './materialize/js/materialize.js';
+
+
+class ServReq extends Component {
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default ServReq;
